33e855b1db208dce4e0570edb81e7d1deea40cc3
Hong Linh <linhle.070801@gmail.com>
2023-10-20 08:21:50 +0700

    Upload first code

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+interface Params {
+  isValid: boolean;
+}
+
+export default function func({ isValid }: Params) {
+  let foo = 1;
+
+  if (isValid) {
+    foo = 2;
+  }
+
+  return foo;
+}
+
+console.log('====================================');
+console.log('run');
+console.log('====================================');
